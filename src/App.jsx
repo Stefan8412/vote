b31e282282b4a7408418c7aeea998405173820ee
Stefan Hancar <stefan@hancar.sk>
2024-11-19 20:39:51 +0100

    add users

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import { AuthProvider } from "./utils/AuthContext";
 import Login from "./pages/Login";
-import Header from "./components/Header";
+
 import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 import Voteweight from "./pages/Vote-weight";
-import Footer from "./components/Footer";
+
 import Layout from "./components/Layout";
 import GDPR from "./pages/GDPRE";
+import UserCounter from "./pages/Users";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route element={<PrivateRoutes />}>
               <Route path="/" element={<Home />} />
+              <Route path="/users" element={<UserCounter />} />
               <Route path="/voteweight" element={<Voteweight />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/gdpr" element={<GDPR />} />
--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -6,6 +6,7 @@ const COLLECTION_ID = "66f0fb8b00386a23dde8";
 const COLLECTION_ID1 = "66fcdeb3002a4ff8016b";
 const COLLECTION_ID2 = "6704281300250441d398";
 const COLLECTION_ID3 = "67042936001d1d7b7a71";
+const COLLECTION_ID4 = "673ae06e00152a243cc2";
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject("66f0f9a4000ca8ced3ad");
@@ -21,5 +22,6 @@ export {
   COLLECTION_ID1,
   COLLECTION_ID2,
   COLLECTION_ID3,
+  COLLECTION_ID4,
   Query,
 };
